cea48fa8967f6ba0a2fc00a400afb1d296f84e18
Stephanie Cushing <stephaniecushing@Stephanies-MacBook-Air.local>
2021-09-01 14:38:01 -0400

    console.logcurrenttimeeverysecond

--- main.js
+++ main.js
@@ -1,6 +1,19 @@
 // 'use strict';
 
 //need class = clock-display to get the current time\
-const date = new Date();
-const time = (date.getHours() +":"+ date.getMinutes() +":"+ date.getSeconds());
+// const date = new Date();
+// const time = (date.getHours() +":"+ date.getMinutes() +":"+ date.getSeconds());
+//     console.log(time);
+
+function updateClock(){
+    const date = new Date();
+    const time = (date.getHours() +":"+ date.getMinutes() +":"+ date.getSeconds());
     console.log(time);
+    
+};
+setInterval(updateClock,1000);
+    
+
+// setInterval(function(){
+//     console.log(time);
+// },1000);
