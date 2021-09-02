90465db0d0b12d29ae0ef6af254567f30e9aa11c
Stephanie Cushing <stephaniecushing@Stephanies-MacBook-Air.local>
2021-09-02 14:22:57 -0400

    Completed Color Clock

--- main.js
+++ main.js
@@ -46,9 +46,19 @@ const $clock = document.querySelector('.clock');
 const $display = document.querySelector('.clock-display');
 
 
-const color =['#DF5069', '#FF0000', '#00FF00','#95D0F0','#E3B44C'];
+const colors =['#DF5069', '#FF0000', '#00FF00','#95D0F0','#E3B44C'];
 let index = 0;
 let isHovering = false; //default for the page
+
+$clock.addEventListener('mouseover', function(){
+    
+    isHovering = true;
+});
+
+$clock.addEventListener('mouseout', function(){
+   
+    isHovering = false;
+});
 
     //function setDate fires EVERY SECOND (see bottom when function is called)
 
@@ -57,20 +67,19 @@ function setDate() {
     let seconds, minutes, hour;
     
     if(isHovering){
+        console.log('hovering');
+        seconds = ('0' + now.getSeconds().toString(16)).slice(-2);
+        minutes = ('0' + now.getMinutes().toString(16)).slice(-2); // number.prototype.toString will convert number to hexdecimal
+        hour = ('0' + now.getHours().toString(16)).slice(-2);
+    } else {
+        console.log('not hovering');
         seconds = ('0' + now.getSeconds()).slice(-2);
         minutes = ('0' + now.getMinutes()).slice(-2); //padding values with O + slice(-2) cutting off :22 of :022
         hour = ('0' + now.getHours()).slice(-2);
-    } else {
-        seconds = ('0' + now.getSeconds().toString()).slice(-2);
-        minutes = ('0' + now.getMinutes().toString()).slice(-2); // number.prototype.toString will convert number to hexdecimal
-        hour = ('0' + now.getHours().toString()).slice(-2);
-        };
+    };
 
-        $display.innerHTML = `${hour}:${minutes}:${seconds}`
-        $progressbar.style.width =  `${seconds/60 * 14}rem`
-    // const color =['#DF5069', '#FF0000', '#00FF00','#95D0F0','#E3B44C'];
-    // let index = 0;
-    // let isHovering = false; //default for the page
+    $display.innerHTML = `${hour}:${minutes}:${seconds}`
+    $progressbar.style.width =  `${seconds/60 * 14}rem`
 
     // $clock.addEventListener('mouseover', function(){
     //     isHovering = true;
@@ -81,20 +90,15 @@ function setDate() {
 };
 
 function setBackground() {
-    // const color =['#DF5069', '#FF0000', '#00FF00','#95D0F0','#E3B44C'];
+    $clock.style.background = colors[index];
+    if(index === colors.length - 1) {
+        index = 0;
+    } else {
+        // index = index + 1;
+        index++;
+    }
+}
 
-    $clock.addEventListener('mouseover', function(){
-        isHovering = true;
-    });
-    $clock.addEventListener('mouseout', function(){
-        isHovering = false;
-    });
-
-    return color
-
-    
-
-    };
 
 setInterval(setDate,1000);
 setInterval(setBackground, 5000);
